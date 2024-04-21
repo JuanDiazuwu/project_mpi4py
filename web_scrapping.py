cce363045b42946ec2909c130f52531e20a5eb38
JUAN ANTONIO DIAZ FERNANDEZ <a348637@uach.mx>
2024-04-20 22:37:15 -0600

    update web scrapping

--- web_scrapping.py
+++ web_scrapping.py
@@ -1,8 +1,29 @@
 import requests
+from mpi4py import MPI
 from bs4 import BeautifulSoup
+from random import randint
 
-URL = "https://realpython.github.io/fake-jobs/"
-page = requests.get(URL)
+URL = "https://pokeapi.co/api/v2/pokemon/"
 
-soup = BeautifulSoup(page.content, "html.parser")
-print(soup)
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+size = comm.Get_size()
+
+def scrape_pokemon_data(aleatori_number):
+    update_url = URL + str(aleatori_number)
+    response = requests.get(update_url).json()
+    pokemon_name = response.get('forms')[0].get('name')
+    return pokemon_name
+
+def main():
+    random_pokemon_number = randint(1, 151)
+    random_pokemon_number = comm.scatter([random_pokemon_number]*size, root=0)
+    pokemon_data = scrape_pokemon_data(random_pokemon_number)
+    all_pokemon_data = comm.gather(pokemon_data, root=0)
+
+    if rank == 0:
+        for data in all_pokemon_data:
+            print(data)
+
+if __name__ == '__main__':
+    main()
