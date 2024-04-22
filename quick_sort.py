0dab75107eda8aee70beef5eb823a666b95ec6f5
JUAN ANTONIO DIAZ FERNANDEZ <a348637@uach.mx>
2024-04-22 16:31:41 -0600

    update quick sort

--- quick_sort.py
+++ quick_sort.py
@@ -1,3 +1,7 @@
+from mpi4py import MPI
+from random import randint
+import math
+
 def partition(array:list, low:int, high:int) -> int:
   pivot = array[high]
   i = low - 1
@@ -16,11 +20,27 @@ def quick_sort(array:list, low:int, high:int) -> list:
     quick_sort(array, low, pi - 1)
     quick_sort(array, pi + 1, high)
 
+def parallel_quicksort(data:list, comm):
+  root = 0
+  max_processors = comm.size
+  new_list = []
+  bin_size = math.floor((max(data) - min(data)) / comm.size)
+  
+  for rank in range(max_processors):
+      new_list.append([x for x in data if (x >= bin_size * rank + rank) and x <= (bin_size + bin_size * rank + rank)])
+  
+  v = comm.scatter(new_list, root)
+  quick_sort(v, 0, len(v) - 1)
+  
+  g = comm.gather(v, root)
+  if comm.rank==0:
+    print([item for sublist in g for item in sublist])
+
 def main():
-    data = [8, 7, 2, 1, 0, 9, 6]
-    print(f'Unsorted Array: {data}')
-    quick_sort(data, 0, len(data)-1)
-    print(f'Sorted Array: {data}')
+  comm = MPI.COMM_WORLD  
+  DATA = [3, 5, 7, 4, 6, 7, 11, 9, 2, 8, 3, 2]
+
+  parallel_quicksort(DATA, comm)
 
 if __name__ == '__main__':
     main()
