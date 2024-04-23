91ef781d21e9524d828832c859137d1dc77b07b9
anahipeinadovi <a353262@uach.mx>
2024-04-22 20:25:40 -0600

    cambios a binary search

--- binary_search.py
+++ binary_search.py
@@ -17,14 +17,13 @@ def main():
     rank = comm.Get_rank()
     size = comm.Get_size()
 
-    print(f'rank:{rank}')
-    print(f'size:{size}')
+
 
     data = []
     for i in range(100):
         data.append(2 * i + 1)  # Genera números impares del 1 al 199
 
-    search_number = 35
+    search_number = random.randint(1,199)
 
 
     # Dividimos el arreglo entre los procesos
@@ -36,16 +35,24 @@ def main():
     local_data = data[start_index:end_index]
 
 
+    print(f'rank:{rank}')
+    print(f'size:{size}')
+    print(f"Arreglo divido del indice {start_index} al indice {end_index}")
+    print(local_data)
+    print("\n")
+
     result = binary_search(local_data, 0, len(local_data) - 1, search_number)
     gathered_results = comm.gather(result, root=0)
+    
 
     # Imprimir resultados en el proceso 0
     if rank == 0:
         final_result = next((i * local_data_size + result for i, result in enumerate(gathered_results) if result != -1), -1)
         if final_result != -1:
-            print(f"El elemento {search_number} está presente en el índice {final_result}.")
+            print(f"El elemento {search_number} SI está presente en el índice {final_result}.")
         else:
-            print("El elemento no está presente en el arreglo.")
+            print(f"El elemento {search_number} NO está presente en el arreglo.")
+
 
 if __name__ == '__main__':
     main()
